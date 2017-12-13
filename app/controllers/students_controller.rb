621b0c09336aca199436b05bd6dfe02fb236b94d
nico24687 <theonlynico@hotmail.com>
2017-12-13 13:19:13 -0700

    creating the index view

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,18 @@ class StudentsController < ApplicationController
   def new 
     @student = Student.new 
   end 
+
+  def index
+    @students = Student.all
+  end 
+
+  def create 
+    @student = Student.create(student_params)
+  end
+
+    private
+
+    def student_params
+      params.require(:student).permit(:name)
+    end 
 end 
