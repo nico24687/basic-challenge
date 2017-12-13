1f3d844668448e5e1651748cd88db34f65203bb6
nico24687 <theonlynico@hotmail.com>
2017-12-13 14:35:31 -0700

    able to see address details for a particular student on show page

---
+++ db/migrate/20171213204923_create_addresses.rb
@@ -0,0 +1,12 @@
+class CreateAddresses < ActiveRecord::Migration[5.1]
+  def change
+    create_table :addresses do |t|
+      t.string :description
+      t.string :street
+      t.string :city
+      t.string :state
+      t.string :zipcode
+      t.references :student, foreign_key: true
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171213200633) do
+ActiveRecord::Schema.define(version: 20171213204923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "description"
+    t.string "street"
+    t.string "city"
+    t.string "state"
+    t.string "zipcode"
+    t.bigint "student_id"
+    t.index ["student_id"], name: "index_addresses_on_student_id"
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "name"
   end
 
+  add_foreign_key "addresses", "students"
 end
