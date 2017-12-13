1a729f450067f86bc413563ce681b88d0173a452
nico24687 <theonlynico@hotmail.com>
2017-12-13 13:28:52 -0700

    able to delete a student

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,9 +7,22 @@ class StudentsController < ApplicationController
     @students = Student.all
   end 
 
+  def show 
+    @student = Student.find(params[:id])
+  end 
+
   def create 
     @student = Student.create(student_params)
+
+    redirect_to student_path(@student)
   end
+
+  def destroy 
+    @student = Student.find(params[:id])
+    @student.destroy
+
+    redirect_to students_path
+  end 
 
     private
 
